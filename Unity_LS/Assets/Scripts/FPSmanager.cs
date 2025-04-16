ad42fc19b7231fe3cc45e5763ba0bce95aa34546
Cheetaumzin <caiotlsaraiva@gmail.com>
2025-04-16 03:18:59 -0300

    feat: Added FPS manager and booster

---
+++ Unity_LS/Assets/Scripts/FPSmanager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+public class FPSmanager : MonoBehaviour
+{
+    [Header("Frame Settings")]
+    int MaxRate = 60;
+    public float TargetFrameRate = 60.0f;
+    float currentFrameTime;
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = MaxRate;
+        currentFrameTime = Time.realtimeSinceStartup;
+        StartCoroutine("WaitForNextFrame");
+    }
+
+    private void Update()
+    {
+        TargetFrameRate = TargetFrameRate + 0;
+    }
+    IEnumerator WaitForNextFrame()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            currentFrameTime += 1.0f / TargetFrameRate;
+            var t = Time.realtimeSinceStartup;
+            var sleepTime = currentFrameTime - t - 0.01f;
+            if (sleepTime > 0)
+                Thread.Sleep((int)(sleepTime * 1000));
+            while (t < currentFrameTime)
+                t = Time.realtimeSinceStartup;
+        }
+    }
+}
